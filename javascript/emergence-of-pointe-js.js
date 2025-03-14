ae818a7f3c3058c3c4f7e01ba1f2034dd9cc31ce
daniellanunzi <dnunziato@ocadu.ca>
2025-03-14 00:26:13 -0400

    Add files via upload

--- javascript/emergence-of-pointe-js.js
+++ javascript/emergence-of-pointe-js.js
@@ -1,6 +1,84 @@
-$(".diagram-area").click(function(){
-    
-    $(".diagram-area").toggleClass("active")
-   
-    console.log("Menu clicked")
+$(document).ready(function(){
+    $("#what-is-pointe-info-1").fadeToggle();
+    $("#line-3").fadeToggle();
+    $("#what-is-pointe-info-2").fadeToggle();
 })
+
+$(".what-is-pointe-container").click(function(){
+    $(".what-is-pointe-container").toggleClass("active");
+    $("#what-is-pointe-info-1").fadeToggle(1);
+    $("#line-3").fadeToggle(1);
+    $("#what-is-pointe-info-2").fadeToggle(1);
+})
+
+$(document).ready(function () {
+    $(".popup-button-1").click(function () {
+        $(".popup-1").fadeIn();
+    });
+    $(".popup-close-1").click(function () {
+        $(".popup-1").fadeOut();
+    });
+});
+
+$(document).ready(function () {
+    $(".popup-button-2").click(function () {
+        $(".popup-2").fadeIn();
+    });
+    $(".popup-close-2").click(function () {
+        $(".popup-2").fadeOut();
+    });
+});
+
+$(document).ready(function () {
+    $(".popup-button-3").click(function () {
+        $(".popup-3").fadeIn();
+    });
+    $(".popup-close-3").click(function () {
+        $(".popup-3").fadeOut();
+    });
+});
+
+$(document).ready(function () {
+    $(".popup-button-4").click(function () {
+        $(".popup-4").fadeIn();
+    });
+    $(".popup-close-4").click(function () {
+        $(".popup-4").fadeOut();
+    });
+});
+
+$(document).ready(function () {
+    $(".popup-button-5").click(function () {
+        $(".popup-5").fadeIn();
+    });
+    $(".popup-close-5").click(function () {
+        $(".popup-5").fadeOut();
+    });
+});
+
+$(document).ready(function () {
+    $(".popup-button-6").click(function () {
+        $(".popup-6").fadeIn();
+    });
+    $(".popup-close-6").click(function () {
+        $(".popup-6").fadeOut();
+    });
+});
+
+$(document).ready(function () {
+    $(".popup-button-7").click(function () {
+        $(".popup-7").fadeIn();
+    });
+    $(".popup-close-7").click(function () {
+        $(".popup-7").fadeOut();
+    });
+});
+
+$(document).ready(function () {
+    $(".popup-button-8").click(function () {
+        $(".popup-8").fadeIn();
+    });
+    $(".popup-close-8").click(function () {
+        $(".popup-8").fadeOut();
+    });
+});
